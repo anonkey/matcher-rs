5c563fd417ecc0452b8ccb1be826642c36029b0f
anonkey <6380129+anonkey@users.noreply.github.com>
2024-03-03 02:14:03 +0100

    ci: p

--- scripts/vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-get_version() {
-  cargo_path="$1"
-  current_version=`toml get $cargo_path package.version`
-  current_version=${current_version//\"/}
-
-  echo "$current_version"
-}
