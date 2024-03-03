005a6abac828cbe774379b652db3f1c42292f50d
anonkey <6380129+anonkey@users.noreply.github.com>
2024-03-03 02:14:03 +0100

    ci: p

--- scripts/init-dev-env.sh
+++ scripts/init-dev-env.sh
@@ -1,6 +1,5 @@
 cargo install conventional_commits_next_version --version ^6
-cargo install conventional_commits_linter
-cargo install git-cliff
+cargo install conventional_commits_linter git-cliff toml-cli
 
 if [ -d .git/hooks ]; then
   for file in git-hooks/*; do
