db2126e65e27e000bf088238da5011330f5aedb7
anonkey <6380129+anonkey@users.noreply.github.com>
2024-03-08 09:54:48 +0100

    ci: p

--- scripts/init-dev-env.sh
+++ scripts/init-dev-env.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+set -vx
+
 cargo install conventional_commits_next_version --version ^6
 cargo install conventional_commits_linter git-cliff toml-cli
 
-if [ -d .git/hooks ]; then
+if [ -d .git/hooks -a -z "$SKIP_GIT_HOOKS" ]; then
   for file in git-hooks/*; do
     ln -s ../../$file .git/hooks/
   done
