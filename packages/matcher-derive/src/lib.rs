a1e2b10c233c298c7e5dffeeb2e0f31ae6c40d59
anonkey <6380129+anonkey@users.noreply.github.com>
2024-03-08 13:52:55 +0100

    feat(tt)!: toto

--- packages/matcher-derive/src/lib.rs
+++ packages/matcher-derive/src/lib.rs
@@ -5,13 +5,10 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(Match, attributes(match_start_with))]
 pub fn matcher_derive(stream: TokenStream) -> TokenStream {
-    // Parse the input tokens into a syntax tree
     let stream_copy = stream.clone();
     let mut item = syn::parse(stream_copy).unwrap();
-    let _ = "";
-    // Build the output, possibly using quasi-quotation
+
     let expanded = impl_matcher_derive(&mut item);
 
-    // Hand the output tokens back to the compiler
     TokenStream::from(expanded)
 }
